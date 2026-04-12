21e1f7ae8969f806e1af62b6b8c01a9546d21512
Petros Stougiannidis <petros.stougiannidis@live.de>
2026-04-12 22:49:22 +0200

    remove token singleton artifact

--- specification/token.py
+++ specification/token.py
@@ -19,20 +19,10 @@ class TokenRegistry:
         return self.tokens.values()  
 
 class Token:
-    _registry = {}
-
+    
     def __init__(self, name, value=None):
         self.type = name
         self.value = value
-        
-    @classmethod
-    def register(cls, name, pattern):
-        ast = parse_regex(pattern)
-        cls._registry[name] = ast
-    
-    @classmethod
-    def get_tokens(cls):
-        return cls._registry.items()    
 
     def __repr__(self):
         if self.value:
